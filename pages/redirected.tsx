b835b5fc347bb8ce35ecdb4ee14b207d941f0a27
Jay Raval <jay.ambrish.raval@gmail.com>
2023-05-16 12:56:01 -0700

    revert prefetch changes

--- pages/redirected.tsx
+++ pages/redirected.tsx
@@ -1,10 +1,6 @@
 import { NextPage } from "next";
-import { useRouter } from "next/router";
 
 export const RedirectedPage: NextPage = () => {
-  const router = useRouter();
-  router.prefetch("/redirected");
-  console.log("prefetching page");
   return <h1>Redirected from /about</h1>;
 };
 
